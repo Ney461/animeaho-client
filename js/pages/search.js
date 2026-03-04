f2ae70a0c6b4012bd9778fcfbad1f5f3016ccf8b
Ney461 <neymd002@gmail.com>
2026-03-04 16:04:43 -0500

    feat: add search results page

--- js/pages/search.js
+++ js/pages/search.js
@@ -2,6 +2,7 @@ import { addListenerInput } from "../utils/search.js";
 import { displayErrorMessage } from "../utils/errorHandler.js";
 import { extractQueryFromURL } from "../utils/urlParams.js";
 import { searchAnimeWithText } from "../services/animeService.js";
+import { createAnimeCard } from "../components/AnimeCard.js";
 
 async function initializeAnimeSearchPage() {
     try {
@@ -10,6 +11,16 @@ async function initializeAnimeSearchPage() {
         const query = extractQueryFromURL();
         const animes = await searchAnimeWithText(query);
         
+        const section = document.querySelector('.main__animes-container');
+
+        animes.data.media.forEach(anime => {
+            const animeCard = createAnimeCard(
+                anime,
+                anime.type,
+                true
+            );
+            section.appendChild(animeCard);
+        });
 
     } catch (error) {
         displayErrorMessage('Error al cargar el contenido. Intenta recargar la página.');
